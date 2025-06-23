7c0dcf182d5c68aaa697ceffa7892f027e26a58c
dehuihu <dehuihu@gmail.com>
2025-06-23 12:12:09 -0700

    Initial setup and creating board and card models

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 import os
 # Import models, blueprints, and anything else needed to set up the app or database
+from .models import board, card
+from .db import db, migrate
 
 
 def create_app(config=None):
@@ -14,6 +16,8 @@ def create_app(config=None):
         app.config.update(config)
 
     # Initialize app with SQLAlchemy db and Migrate
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register Blueprints 
 
