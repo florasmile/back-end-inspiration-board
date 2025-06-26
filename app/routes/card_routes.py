3e2e8dbd9444a6d180b25f6e85006de4fcbabd26
Mikaela Baluyot <mikaebal@gmail.com>
2025-06-26 09:00:55 -0700

    refactored card routes, added tests

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -4,11 +4,11 @@ from ..models.board import Board
 from ..models.card import Card
 from .helpers import validate_model, create_model
 
-# all routes end with /cards
-bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
+
+bp = Blueprint("cards_bp", __name__)
 
 
-# GET /cards/boards/1/cards
+# GET /boards/1/cards
 # get all cards for a specific board
 @bp.get("/boards/<int:board_id>/cards")
 def get_cards_for_board(board_id):
@@ -22,7 +22,7 @@ def get_cards_for_board(board_id):
     return [card.to_dict() for card in cards]
 
 
-# POST /cards/boards/1/cards
+# POST /boards/1/cards
 # create a card for a specific board
 @bp.post("/boards/<int:board_id>/cards")
 def create_card_for_board(board_id):
@@ -41,7 +41,7 @@ def create_card_for_board(board_id):
 
 # DELETE /cards/1 
 # delete a card by id
-@bp.delete("/<int:card_id>")
+@bp.delete("/cards/<int:card_id>")
 def delete_card(card_id):
     # helper func
     card = validate_model(Card, card_id)
@@ -56,7 +56,7 @@ def delete_card(card_id):
 
 # PATCH /cards/1/like
 # like a card by id (increment likes_count)
-@bp.patch("/<int:card_id>/like")
+@bp.patch("/cards/<int:card_id>/like")
 def like_card(card_id):
     # helper func
     card = validate_model(Card, card_id)
